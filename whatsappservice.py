3138016a87fc10dfc24ec5ddf7dca00f71172995
EmilioSantosCastillo <santosemilio@gmail.com>
2024-07-19 09:12:41 -0400

    ocultar token

--- whatsappservice.py
+++ whatsappservice.py
@@ -1,9 +1,11 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
 
 def SendMessageWhatsapp(textUser,number):
     try:
-        token="EAAQgVrW8ObgBO67hnFMP1pBKW3kHGp9ZBHyFBCJVEIZAeH8cncr5vFg2losUZB9JZCj7TS69vffKQTellYON1KasqtrSZAEKzMCrQZCHqrWe9seOYRh4BfCO1VZAK7kTYPy9raovxSt084NQ7bvK62Qgs7WmNRznXDvW5Y3eYWOEWOfJ6Np6tuh7uLminS0R7KxpbMSmZB2KaFkEh1K8rvmwsAzGbuN9egZC8nVLpc80ZD"
+        token = os.getenv("WHATSAPP_API_TOKEN")
         api_url="https://graph.facebook.com/v19.0/370015532861574/messages"
         data={
                 "messaging_product": "whatsapp",    
